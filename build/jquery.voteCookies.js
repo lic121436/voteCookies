903a486a5bd537d3b243f819f89b45e1f0d74339
lic121436@163.com <lic121436@163.com>
2018-08-15 17:38:52 +0800

    主要插件修改

--- build/jquery.voteCookies.js
+++ build/jquery.voteCookies.js
@@ -7,7 +7,7 @@
 		exports["voteCookies"] = factory();
 	else
 		root["voteCookies"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
@@ -81,24 +81,80 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 
-function voteCookies() {
+function voteCookies(options) {
 
 };
 
-/**
+// 设置voteCookies默认属性
+voteCookies.DEFAULTS = {
+	cookieName: "", // cookie名称
+	voteBtnSelectorAll: "", // 获取投票按钮的DOM
+	showNumSelector: "", // 获取显示投票数量的DOM
+	maxNum: 9, // 最大投票数量
+	minNum: 5, // 最小投票数量
+	layerType: "layerPc", // 弹窗类型 default: alert弹窗, layerPc: layer.alert弹窗(需加载layer.js pc版本)， layerMobile: layer.open弹窗(需加载layer.js 移动版本)
+};
+
+voteCookies.prototype.cookieInit = function (options) {
+	var opts = Object.assign(voteCookies.DEFAULTS, options); // ES6 对象拷贝
+	var self = this;
+	var clickAndSelectBtns = document.querySelectorAll(opts.voteBtnSelectorAll), // 获取投票按钮的DOM
+		showNumText = document.querySelector(opts.showNumSelector), // 获取显示投票数量的DOM
+		attrName = 'data-id', // 投票选中时获取存入cookie值的属性名称
+		selectedShowHtml = '已选中', // 按钮显示的html
+		showHtml = '投票'; // 选中后按钮显示的html
+
+	// 获取cookie并将其分割成数组
+	var showNumArr = this.splitCookies(this.getCookies(opts.cookieName));
+	// 根据cookie显示选中的按钮与数量
+	this.showNum(opts.cookieName, clickAndSelectBtns, clickAndSelectBtns, showNumText,
+		attrName,
+		selectedShowHtml);
+
+	// 点击按钮时选中或都去除选中按钮，并修改按钮样式，与显示数量
+	for (var i = 0; i < clickAndSelectBtns.length; i++) {
+		(function (i) {
+			var thisBtn = clickAndSelectBtns[i],
+				thisCookieName = opts.cookieName,
+				thisMaxNum = opts.maxNum,
+				thisMinNum = opts.minNum,
+				thisLayerType = opts.layerType;
+
+			thisBtn.onclick = function () {
+				self.clickBtnSelect(
+					attrName,
+					thisBtn,
+					showNumText,
+					showNumArr,
+					thisMaxNum,
+					thisMinNum,
+					thisCookieName,
+					showHtml,
+					selectedShowHtml,
+					thisLayerType
+				);
+			}
+
+		})(i);
+	}
+	return showNumArr;
+};
+
+
+/**CanvasTitle
  * 判断cookie是否存在，存在则返回cookie值，不存在返回空数组
  * @param {*} cookieName 
  */
 
-voteCookies.prototype.getCookies = function(cookieName) {
+voteCookies.prototype.getCookies = function (cookieName) {
 	var arr = document.cookie.replace(/\s/g, "").split(';');
-    for (var i = 0; i < arr.length; i++) {
+	for (var i = 0; i < arr.length; i++) {
 		var tempArr = arr[i].split('=');
-        if (tempArr[0] === cookieName) {
-            return decodeURIComponent(tempArr[1]);
-        }
-    }
-    return [];
+		if (tempArr[0] === cookieName) {
+			return decodeURIComponent(tempArr[1]);
+		}
+	}
+	return [];
 };
 
 /**
@@ -146,10 +202,10 @@ voteCookies.prototype.showNum = function (cookieName, getAllBtnList, getAllListS
 	// 判断cookieArr数组是否存在，若是存在将数组里的值跟对应的范围的值标签进行更改(此处改变对应属性相同的标签内的html值),同时改变对应显示数量
 	if (cookieArr) {
 		for (var i = 0; i < cookieArr.length; i++) {
-			(function(i){
-				for(var j = 0; j < getAllListSelector.length; j++){
-					
-					if(getAllListSelector[j].getAttribute(attrName) === cookieArr[i]){
+			(function (i) {
+				for (var j = 0; j < getAllListSelector.length; j++) {
+
+					if (getAllListSelector[j].getAttribute(attrName) === cookieArr[i]) {
 						getAllListSelector[j].innerHTML = selectedShowHtml;
 					}
 				}
@@ -172,15 +228,15 @@ voteCookies.prototype.showNum = function (cookieName, getAllBtnList, getAllListS
  * @param {*} showHtml       		//  按钮显示的html
  * @param {*} selectedShowHtml     //  选中后按钮显示的html
  */
-voteCookies.prototype.clickBtnSelect = function (attrName, clickSelector, showNumSelector, showNumArr, voteMaxNum, voteMinNum, cookieName, selectedShowHtml, showHtml) {
+voteCookies.prototype.clickBtnSelect = function (attrName, clickSelector, showNumSelector, showNumArr, voteMaxNum, voteMinNum, cookieName, selectedShowHtml, showHtml, layerType) {
 	// 获取属性属性值
-	var valId = clickSelector.getAttribute(attrName); 
+	var valId = clickSelector.getAttribute(attrName);
 
 	// 判断当前的属性值是否在数组中，不在数组中将其加入数组，并改变它的html内容；在数组中则其从数组中删除，并改变它的html内容。
 	if (showNumArr.indexOf(valId) === -1) {
-		this._isMaxNum(showNumSelector, showNumArr, voteMaxNum, valId, clickSelector, showHtml);
+		this._isMaxNum(showNumSelector, showNumArr, voteMaxNum, valId, clickSelector, showHtml, layerType);
 	} else {
-		this._isMinNum(showNumSelector, showNumArr, voteMinNum, valId, clickSelector, selectedShowHtml);
+		this._isMinNum(showNumSelector, showNumArr, voteMinNum, valId, clickSelector, selectedShowHtml, layerType);
 	}
 
 	// 设置cookie
@@ -195,10 +251,10 @@ voteCookies.prototype.clickBtnSelect = function (attrName, clickSelector, showNu
  * @param {*} value // 传入的cookie的值
  * @param {*} days // 传入的过期天数
  */
-voteCookies.prototype.setCookie = function(cookieName, value, days) {
-    var date = new Date();
-    date.setDate(date.getDate() + days);
-    document.cookie = cookieName + '=' + value.join('|') + ';expires=' + date;
+voteCookies.prototype.setCookie = function (cookieName, value, days) {
+	var date = new Date();
+	date.setDate(date.getDate() + days);
+	document.cookie = cookieName + '=' + value.join('|') + ';expires=' + date;
 };
 
 /**
@@ -209,7 +265,7 @@ voteCookies.prototype.setCookie = function(cookieName, value, days) {
  * @param {*} valId 	 	 //  当前的属性值
  * @param {*} $this 	 	 //  当前的点击的DOM（按钮）
  */
-voteCookies.prototype._isMaxNum = function (showNumSelector, showNumArr, voteMaxNum, valId, $this, selectedShowHtml) {
+voteCookies.prototype._isMaxNum = function (showNumSelector, showNumArr, voteMaxNum, valId, $this, selectedShowHtml, layerType) {
 	if (showNumArr.length <= (voteMaxNum - 1)) {
 		showNumArr.push(valId);
 		// 对其改变样式html
@@ -217,10 +273,8 @@ voteCookies.prototype._isMaxNum = function (showNumSelector, showNumArr, voteMax
 		// 改变对应显示数量
 		showNumSelector.innerHTML = showNumArr.length;
 	} else {
-		alert("最大票数不能超过: " + voteMaxNum + "票", {
-			title: "最大票数提示",
-			icon: 5
-		});
+		// 弹出最小票数提示
+		this._layer(layerType, "最大票数提示", "最大票数不能超过", voteMaxNum);
 	}
 };
 
@@ -233,7 +287,7 @@ voteCookies.prototype._isMaxNum = function (showNumSelector, showNumArr, voteMax
  * @param {*} $this 	 	 //  当前的点击的DOM（按钮）
  */
 
-voteCookies.prototype._isMinNum = function (showNumSelector, showNumArr, voteMinNum, valId, $this, showHtml) {
+voteCookies.prototype._isMinNum = function (showNumSelector, showNumArr, voteMinNum, valId, $this, showHtml, layerType) {
 	if (showNumArr.length > voteMinNum) {
 		// 从数组中将当前属性值删除
 		showNumArr.splice(showNumArr.indexOf(valId), 1);
@@ -242,12 +296,47 @@ voteCookies.prototype._isMinNum = function (showNumSelector, showNumArr, voteMin
 		// 改变对应显示数量
 		showNumSelector.innerHTML = showNumArr.length;
 	} else {
-		alert("最小票数不能小于: " + voteMinNum + "票", {
-			title: "最小票数提示",
-			icon: 5
-		});
+		// 弹出最小票数提示
+		this._layer(layerType, "最小票数提示", "最小票数不能小于", voteMinNum);
 	}
 };
+
+voteCookies.prototype.operation = {
+	default: function (title, contentText, number) {
+		alert(contentText + number + "票");
+	},
+	layerPc: function (title, contentText, number) {
+		layer.alert(contentText + number + "票", {
+			title: title,
+			icon: 5
+		});
+	},
+	layerMobile: function (title, contentText, number) {
+		layer.open({
+			title: title,
+			content: contentText + number + "票",
+			btn: '确定'
+		});
+	},
+	addOperation: function (name, fn) {
+		if (!this[name]) {
+			this[name] = fn;
+		}
+		return this;
+	}
+};
+
+/**
+ * 设置弹出窗
+ * @param {number} layerType 弹窗类型
+ * @param {string} title 弹窗标题
+ * @param {string} contentText 弹窗文字内容
+ * @param {number} number 参数
+ */
+voteCookies.prototype._layer = function (layerType, title, contentText, number) {
+
+	this.operation[layerType](title, contentText, number)
+}
 
 /**
  * 获取getAllListSelector所包含的属性标签并将其attr生成一个数组getAllListSelectorArr返回
@@ -256,7 +345,7 @@ voteCookies.prototype._isMinNum = function (showNumSelector, showNumArr, voteMin
  * @param {*} attrName   		 获取vaId的属性名称
  */
 voteCookies.prototype._getDataIdArr = function (getAllListSelectorArr, getAllListSelector, attrName) {
-	for(var i = 0; i < getAllListSelector.length; i++){
+	for (var i = 0; i < getAllListSelector.length; i++) {
 		getAllListSelectorArr.push(getAllListSelector[i].getAttribute(attrName));
 	}
 	return getAllListSelectorArr;
@@ -286,8 +375,6 @@ voteCookies.prototype._setNewArr = function (cookieArr, getAllListSelectorArr) {
 function createVoteCookies() {
 	return new voteCookies();
 }
-
-
 
 module.exports = createVoteCookies;
 
